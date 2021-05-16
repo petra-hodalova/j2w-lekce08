686ea5d95cd1dde736ab34009770cf0605f76cfa
Filip Jirsák <filip@jirsak.org>
2021-05-17 00:32:01 +0200

    Řešení s použitím databáze.

--- src/main/java/cz/czechitas/java2webapps/lekce8/controller/OsobaController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce8/controller/OsobaController.java
@@ -1,7 +1,10 @@
 package cz.czechitas.java2webapps.lekce8.controller;
 
 import cz.czechitas.java2webapps.lekce8.entity.Osoba;
+import cz.czechitas.java2webapps.lekce8.repository.OsobaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
@@ -13,15 +16,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class OsobaController {
 
-  private final List<Osoba> seznamOsob = List.of(
-          new Osoba(1L, "Božena", "Němcová", LocalDate.of(1820, 2, 4), "Vídeň", null, null)
-  );
+  private final OsobaRepository repository;
+
+  @Autowired
+  public OsobaController(OsobaRepository repository) {
+    this.repository = repository;
+  }
 
   @InitBinder
   public void nullStringBinding(WebDataBinder binder) {
@@ -33,7 +38,7 @@ public class OsobaController {
   public Object seznam() {
     //TODO načíst seznam osob
     return new ModelAndView("seznam")
-            .addObject("osoby", seznamOsob);
+            .addObject("osoby", repository.findAll());
   }
 
   @GetMapping("/novy")
@@ -47,15 +52,19 @@ public class OsobaController {
     if (bindingResult.hasErrors()) {
       return "detail";
     }
-    //TODO uložit údaj o nové osobě
+    osoba.setId(null);
+    repository.save(osoba);
     return "redirect:/";
   }
 
   @GetMapping("/{id:[0-9]+}")
   public Object detail(@PathVariable long id) {
-    //TODO načíst údaj o osobě
+    Optional<Osoba> osoba = repository.findById(id);
+    if (osoba.isEmpty()) {
+      return ResponseEntity.notFound();
+    }
     return new ModelAndView("detail")
-            .addObject("osoba", seznamOsob.get(0));
+            .addObject("osoba", osoba.get());
   }
 
   @PostMapping("/{id:[0-9]+}")
@@ -63,13 +72,14 @@ public class OsobaController {
     if (bindingResult.hasErrors()) {
       return "detail";
     }
-    //TODO uložit údaj o osobě
+    osoba.setId(id);
+    repository.save(osoba);
     return "redirect:/";
   }
 
   @PostMapping(value = "/{id:[0-9]+}", params = "akce=smazat")
   public Object smazat(@PathVariable long id) {
-    //TODO smazat údaj o osobě
+    repository.deleteById(id);
     return "redirect:/";
   }
 
---
+++ src/main/java/cz/czechitas/java2webapps/lekce8/repository/OsobaRepository.java
@@ -0,0 +1,12 @@
+package cz.czechitas.java2webapps.lekce8.repository;
+
+import cz.czechitas.java2webapps.lekce8.entity.Osoba;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository pro přístup k databázové tabulce {@code osoba}.
+ */
+@Repository
+public interface OsobaRepository extends CrudRepository<Osoba, Long> {
+}
